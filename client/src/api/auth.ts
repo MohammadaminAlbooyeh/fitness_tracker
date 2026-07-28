6a5d33c103f28444cb6acb30986a8a454f199678
Amin Albooyeh <amin.albooyeh@gmail.com>
2026-07-28 16:34:07 +0200

    initialize scaleable ecommerce platform project

--- client/src/api/auth.ts
+++
@@ -1,7 +0,0 @@
-export const getAuthHeaders = async () => {
-  const token = localStorage.getItem('token');
-  return {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json',
-  };
-};
